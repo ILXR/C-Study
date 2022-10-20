582989bdd74dd56abb9d2872663d213174b65e17
gyc wsl <gyc8787@163.com>
2022-10-20 20:17:55 +0800

    更新lru

--- lru_cache.cc
+++ lru_cache.cc
@@ -1,18 +1,10 @@
-// #include <iostream>
-// #include <list>
-// #include <unordered_map>
-// #include <memory>
-// #include <mutex>
-// #include <functional>
-
-// using namespace std;
-
 #include <iostream>
 #include <list>
 #include <unordered_map>
 #include <memory>
 #include <mutex>
 using namespace std;
+
 template <typename K, typename V>
 class LRU_Cache
 {
@@ -56,43 +48,6 @@ private:
     list<node> lru_list;
     unordered_map<K, iterator> lru_map;
 };
-
-// template <typename K, typename V>
-// class LRU_Cache
-// {
-// public:
-//     typedef pair<K, shared_ptr<V>> node;
-//     typedef typename list<node>::iterator iterator;
-//     LRU_Cache(int cap) : capacity(cap) {}
-//     void insert(K key, shared_ptr<V> val)
-//     {
-//         lock_guard<mutex> guard(mutex_t);
-//         auto ite = lru_map.find(key);
-//         if (ite != lru_map.end())
-//         {
-//             lru_list.erase(ite->second);
-//         }
-//         lru_list.emplace_front(key, val);
-//         lru_map[key] = lru_list.begin();
-//         if (lru_list.size() > capacity)
-//         {
-//             lru_map.erase(lru_list.back().first);
-//             lru_list.pop_back();
-//         }
-//     }
-//     shared_ptr<V> get(K key)
-//     {
-//         lock_guard<mutex> guard(mutex_t);
-//         auto ite = lru_map.find(key);
-//         return ite == lru_map.end() ? shared_ptr<V>() : ite->second->second;
-//     }
-
-// private:
-//     int capacity;
-//     mutex mutex_t;
-//     list<node> lru_list;
-//     unordered_map<K, iterator> lru_map;
-// };
 
 int main()
 {
